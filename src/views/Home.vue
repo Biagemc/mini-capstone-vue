d93560f27a43391ee9c605577e0f6004456a85ce
biagemc <biagemc@gmail.com>
2020-04-20 21:50:42 -0400

    add index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,50 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div class="row">
+      <div class="col-xl-4" v-bind:key="product.id" v-for="product in products">
+        <b-card
+          v-bind:title="product.name"
+          img-src="product.images.url"
+          img-alt="product-image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="card mb-2 col-xl-3 "
+        >
+          <b-card-text>
+            {{ product.description }}
+          </b-card-text>
+          <p>Price: {{ product.price }}</p>
+          <b-button href="#" variant="primary">Show Info</b-button>
+        </b-card>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome to Mini Capstone Products",
+      products: [],
     };
   },
-  created: function() {},
+  created: function() {
+    axios
+      .get("/api/products")
+      .then(response => {
+        console.log(response.data);
+        this.products = response.data;
+      })
+      .catch(error => {
+        this.errors = error.response.data.errors;
+      });
+  },
   methods: {},
 };
 </script>
---
+++ src/views/ProductsNew.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div>
+      <div class="mt-2">Name: {{ name }}</div>
+      <b-form-input v-model="name"></b-form-input>
+    </div>
+    <div>
+      <div class="mt-2">Description: {{ description }}</div>
+      <b-form-input v-model="Description"></b-form-input>
+    </div>
+    <div>
+      <div class="mt-2">Price: {{ price }}</div>
+      <b-form-input v-model="price"></b-form-input>
+    </div>
+    <div>
+      <div class="mt-2">Supplier Id: {{ supplierId }}</div>
+      <b-form-input v-model="supplierId"></b-form-input>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      message: "Products New!",
+      name: "",
+      description: "",
+      price: "",
+      supplierId: "",
+    };
+  },
+  created: function() {},
+  methods: {},
+};
+</script>
